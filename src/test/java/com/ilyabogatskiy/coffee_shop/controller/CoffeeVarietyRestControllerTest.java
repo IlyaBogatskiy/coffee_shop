c94830af7676f9b57a8269454873e97794eff417
IlyaBogatskiy <ilya.bogatskiy@gmail.com>
2022-06-22 10:14:39 +0300

    Подправил тесты.

--- src/test/java/com/ilyabogatskiy/coffee_shop/controller/CoffeeVarietyRestControllerTest.java
+++ src/test/java/com/ilyabogatskiy/coffee_shop/controller/CoffeeVarietyRestControllerTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ilyabogatskiy.coffee_shop.dto.CoffeeVarietyDto;
-import com.ilyabogatskiy.coffee_shop.mapper.CoffeeVarietyMapper;
 import com.ilyabogatskiy.coffee_shop.models.CoffeeVariety;
 import com.ilyabogatskiy.coffee_shop.service.Impl.CoffeeVarietyServiceImpl;
 
@@ -28,16 +27,15 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @ContextConfiguration(classes = {CoffeeVarietyRestController.class})
 @ExtendWith(SpringExtension.class)
 class CoffeeVarietyRestControllerTest {
-
-    @MockBean
-    private CoffeeVarietyMapper coffeeVarietyMapper;
-
     @Autowired
     private CoffeeVarietyRestController coffeeVarietyRestController;
 
     @MockBean
     private CoffeeVarietyServiceImpl coffeeVarietyServiceImpl;
 
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#addCoffeeVariety(CoffeeVarietyDto)}
+     */
     @Test
     void testAddCoffeeVariety() throws Exception {
         CoffeeVariety coffeeVariety = new CoffeeVariety();
@@ -47,18 +45,11 @@ class CoffeeVarietyRestControllerTest {
         coffeeVariety.setPrice(BigDecimal.valueOf(42L));
         when(this.coffeeVarietyServiceImpl.add((CoffeeVariety) any())).thenReturn(coffeeVariety);
 
-        CoffeeVariety coffeeVariety1 = new CoffeeVariety();
-        coffeeVariety1.setAvailable(true);
-        coffeeVariety1.setId(123L);
-        coffeeVariety1.setName("Name");
-        coffeeVariety1.setPrice(BigDecimal.valueOf(42L));
-        when(this.coffeeVarietyMapper.toModel((CoffeeVarietyDto) any())).thenReturn(coffeeVariety1);
-
         CoffeeVarietyDto coffeeVarietyDto = new CoffeeVarietyDto();
         coffeeVarietyDto.setAvailable(true);
         coffeeVarietyDto.setId(123L);
         coffeeVarietyDto.setName("Name");
-        coffeeVarietyDto.setPrice("Price");
+        coffeeVarietyDto.setPrice("42");
         String content = (new ObjectMapper()).writeValueAsString(coffeeVarietyDto);
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/variety/add")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -72,35 +63,36 @@ class CoffeeVarietyRestControllerTest {
                         MockMvcResultMatchers.content().string("{\"id\":123,\"name\":\"Name\",\"price\":42,\"available\":true}"));
     }
 
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#deleteCoffeeVarietyById(Long)}
+     */
     @Test
     void testDeleteCoffeeVarietyById() throws Exception {
         doNothing().when(this.coffeeVarietyServiceImpl).delete((Long) any());
-
-        CoffeeVariety coffeeVariety = new CoffeeVariety();
-        coffeeVariety.setAvailable(true);
-        coffeeVariety.setId(123L);
-        coffeeVariety.setName("Name");
-        coffeeVariety.setPrice(BigDecimal.valueOf(42L));
-        when(this.coffeeVarietyMapper.toModel((CoffeeVarietyDto) any())).thenReturn(coffeeVariety);
-
-        CoffeeVarietyDto coffeeVarietyDto = new CoffeeVarietyDto();
-        coffeeVarietyDto.setAvailable(true);
-        coffeeVarietyDto.setId(123L);
-        coffeeVarietyDto.setName("Name");
-        coffeeVarietyDto.setPrice("Price");
-        String content = (new ObjectMapper()).writeValueAsString(coffeeVarietyDto);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/variety/delete/{id}", 123L)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/variety/delete/{id}", 123L);
         MockMvcBuilders.standaloneSetup(this.coffeeVarietyRestController)
                 .build()
                 .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(
-                        MockMvcResultMatchers.content().string("{\"id\":123,\"name\":\"Name\",\"price\":42,\"available\":true}"));
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#deleteCoffeeVarietyById(Long)}
+     */
+    @Test
+    void testDeleteCoffeeVarietyById2() throws Exception {
+        doNothing().when(this.coffeeVarietyServiceImpl).delete((Long) any());
+        MockHttpServletRequestBuilder deleteResult = MockMvcRequestBuilders.delete("/variety/delete/{id}", 123L);
+        deleteResult.contentType("https://example.org/example");
+        MockMvcBuilders.standaloneSetup(this.coffeeVarietyRestController)
+                .build()
+                .perform(deleteResult)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#getAllAvailableCoffeeVarieties()}
+     */
     @Test
     void testGetAllAvailableCoffeeVarieties() throws Exception {
         when(this.coffeeVarietyServiceImpl.findAllAvailable()).thenReturn(new ArrayList<>());
@@ -113,6 +105,9 @@ class CoffeeVarietyRestControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("[]"));
     }
 
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#getAllAvailableCoffeeVarieties()}
+     */
     @Test
     void testGetAllAvailableCoffeeVarieties2() throws Exception {
         when(this.coffeeVarietyServiceImpl.findAllAvailable()).thenReturn(new ArrayList<>());
@@ -126,6 +121,9 @@ class CoffeeVarietyRestControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("[]"));
     }
 
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#getAllCoffeeVarieties()}
+     */
     @Test
     void testGetAllCoffeeVarieties() throws Exception {
         when(this.coffeeVarietyServiceImpl.findAll()).thenReturn(new ArrayList<>());
@@ -138,6 +136,9 @@ class CoffeeVarietyRestControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("[]"));
     }
 
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#getAllCoffeeVarieties()}
+     */
     @Test
     void testGetAllCoffeeVarieties2() throws Exception {
         when(this.coffeeVarietyServiceImpl.findAll()).thenReturn(new ArrayList<>());
@@ -151,6 +152,9 @@ class CoffeeVarietyRestControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("[]"));
     }
 
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#getCoffeeVarietyById(Long)}
+     */
     @Test
     void testGetCoffeeVarietyById() throws Exception {
         CoffeeVariety coffeeVariety = new CoffeeVariety();
@@ -159,23 +163,7 @@ class CoffeeVarietyRestControllerTest {
         coffeeVariety.setName("Name");
         coffeeVariety.setPrice(BigDecimal.valueOf(42L));
         when(this.coffeeVarietyServiceImpl.findById((Long) any())).thenReturn(coffeeVariety);
-
-        CoffeeVariety coffeeVariety1 = new CoffeeVariety();
-        coffeeVariety1.setAvailable(true);
-        coffeeVariety1.setId(123L);
-        coffeeVariety1.setName("Name");
-        coffeeVariety1.setPrice(BigDecimal.valueOf(42L));
-        when(this.coffeeVarietyMapper.toModel((CoffeeVarietyDto) any())).thenReturn(coffeeVariety1);
-
-        CoffeeVarietyDto coffeeVarietyDto = new CoffeeVarietyDto();
-        coffeeVarietyDto.setAvailable(true);
-        coffeeVarietyDto.setId(123L);
-        coffeeVarietyDto.setName("Name");
-        coffeeVarietyDto.setPrice("Price");
-        String content = (new ObjectMapper()).writeValueAsString(coffeeVarietyDto);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/variety/find/{id}", 123L)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/variety/find/{id}", 123L);
         MockMvcBuilders.standaloneSetup(this.coffeeVarietyRestController)
                 .build()
                 .perform(requestBuilder)
@@ -185,6 +173,31 @@ class CoffeeVarietyRestControllerTest {
                         MockMvcResultMatchers.content().string("{\"id\":123,\"name\":\"Name\",\"price\":42,\"available\":true}"));
     }
 
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#getCoffeeVarietyById(Long)}
+     */
+    @Test
+    void testGetCoffeeVarietyById2() throws Exception {
+        CoffeeVariety coffeeVariety = new CoffeeVariety();
+        coffeeVariety.setAvailable(true);
+        coffeeVariety.setId(123L);
+        coffeeVariety.setName("Name");
+        coffeeVariety.setPrice(BigDecimal.valueOf(42L));
+        when(this.coffeeVarietyServiceImpl.findById((Long) any())).thenReturn(coffeeVariety);
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/variety/find/{id}", 123L);
+        getResult.contentType("https://example.org/example");
+        MockMvcBuilders.standaloneSetup(this.coffeeVarietyRestController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(
+                        MockMvcResultMatchers.content().string("{\"id\":123,\"name\":\"Name\",\"price\":42,\"available\":true}"));
+    }
+
+    /**
+     * Method under test: {@link CoffeeVarietyRestController#updateCoffeeVarieties(CoffeeVarietyDto)}
+     */
     @Test
     void testUpdateCoffeeVarieties() throws Exception {
         CoffeeVariety coffeeVariety = new CoffeeVariety();
@@ -194,18 +207,11 @@ class CoffeeVarietyRestControllerTest {
         coffeeVariety.setPrice(BigDecimal.valueOf(42L));
         when(this.coffeeVarietyServiceImpl.edit((CoffeeVariety) any())).thenReturn(coffeeVariety);
 
-        CoffeeVariety coffeeVariety1 = new CoffeeVariety();
-        coffeeVariety1.setAvailable(true);
-        coffeeVariety1.setId(123L);
-        coffeeVariety1.setName("Name");
-        coffeeVariety1.setPrice(BigDecimal.valueOf(42L));
-        when(this.coffeeVarietyMapper.toModel((CoffeeVarietyDto) any())).thenReturn(coffeeVariety1);
-
         CoffeeVarietyDto coffeeVarietyDto = new CoffeeVarietyDto();
         coffeeVarietyDto.setAvailable(true);
         coffeeVarietyDto.setId(123L);
         coffeeVarietyDto.setName("Name");
-        coffeeVarietyDto.setPrice("Price");
+        coffeeVarietyDto.setPrice("42");
         String content = (new ObjectMapper()).writeValueAsString(coffeeVarietyDto);
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/variety/update")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -219,3 +225,4 @@ class CoffeeVarietyRestControllerTest {
                         MockMvcResultMatchers.content().string("{\"id\":123,\"name\":\"Name\",\"price\":42,\"available\":true}"));
     }
 }
+
--- src/test/java/com/ilyabogatskiy/coffee_shop/controller/OrderRestControllerTest.java
+++ src/test/java/com/ilyabogatskiy/coffee_shop/controller/OrderRestControllerTest.java
@@ -2,23 +2,27 @@ package com.ilyabogatskiy.coffee_shop.controller;
 
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ilyabogatskiy.coffee_shop.dto.CoffeeVarietyDto;
 import com.ilyabogatskiy.coffee_shop.dto.OrderDto;
 import com.ilyabogatskiy.coffee_shop.dto.OrderItemDto;
-import com.ilyabogatskiy.coffee_shop.mapper.OrderItemMapper;
-import com.ilyabogatskiy.coffee_shop.mapper.OrderMapper;
 import com.ilyabogatskiy.coffee_shop.models.CoffeeVariety;
 import com.ilyabogatskiy.coffee_shop.models.Order;
 import com.ilyabogatskiy.coffee_shop.models.OrderItem;
+import com.ilyabogatskiy.coffee_shop.repository.OrderItemRepository;
+import com.ilyabogatskiy.coffee_shop.repository.OrderRepository;
 import com.ilyabogatskiy.coffee_shop.service.Impl.OrderServiceImpl;
+import com.ilyabogatskiy.coffee_shop.service.OrderPriceCalculationService;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,12 +39,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @ExtendWith(SpringExtension.class)
 class OrderRestControllerTest {
 
-    @MockBean
-    private OrderItemMapper orderItemMapper;
-
-    @MockBean
-    private OrderMapper orderMapper;
-
     @Autowired
     private OrderRestController orderRestController;
 
@@ -48,24 +46,9 @@ class OrderRestControllerTest {
     private OrderServiceImpl orderServiceImpl;
 
     @Test
-    void testCreateOrder() throws Exception {
-        Order order = new Order();
-        order.setCustomerName("Customer Name");
-        order.setDeliveryAddress("42 Main St");
-        order.setId(123L);
-        order.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
-        order.setOrderItems(new ArrayList<>());
-        order.setOrderPrice(BigDecimal.valueOf(42L));
-        when(this.orderServiceImpl.add((Order) any())).thenReturn(order);
-
-        Order order1 = new Order();
-        order1.setCustomerName("Customer Name");
-        order1.setDeliveryAddress("42 Main St");
-        order1.setId(123L);
-        order1.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
-        order1.setOrderItems(new ArrayList<>());
-        order1.setOrderPrice(BigDecimal.valueOf(42L));
-        when(this.orderMapper.toModel((OrderDto) any())).thenReturn(order1);
+    void testCreateOrder() {
+        OrderRestController orderRestController = new OrderRestController(new OrderServiceImpl(mock(OrderRepository.class),
+                mock(OrderItemRepository.class), mock(OrderPriceCalculationService.class)));
 
         OrderDto orderDto = new OrderDto();
         orderDto.setCustomerName("Customer Name");
@@ -74,19 +57,97 @@ class OrderRestControllerTest {
         orderDto.setOrderDateTime("2020-03-01");
         orderDto.setOrderItemDtos(new ArrayList<>());
         orderDto.setOrderPrice("Order Price");
-        String content = (new ObjectMapper()).writeValueAsString(orderDto);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/order/create")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
-        MockMvcBuilders.standaloneSetup(this.orderRestController)
-                .build()
-                .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string(
-                                "{\"id\":123,\"orderDateTime\":[1,1,1,1,1],\"customerName\":\"Customer Name\",\"deliveryAddress\":\"42 Main"
-                                        + " St\",\"orderPrice\":42,\"orderItems\":[]}"));
+        orderRestController.createOrder(orderDto);
+    }
+
+    @Test
+    void testCreateOrder2() {
+        Order order = new Order();
+        order.setCustomerName("Customer Name");
+        order.setDeliveryAddress("42 Main St");
+        order.setId(123L);
+        order.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
+        order.setOrderItems(new ArrayList<>());
+        order.setOrderPrice(BigDecimal.valueOf(42L));
+        OrderRepository orderRepository = mock(OrderRepository.class);
+        when(orderRepository.saveAndFlush((Order) any())).thenReturn(order);
+        OrderPriceCalculationService orderPriceCalculationService = mock(OrderPriceCalculationService.class);
+        when(orderPriceCalculationService.orderPriceCalculation((Order) any())).thenReturn(BigDecimal.valueOf(42L));
+
+        Order order1 = new Order();
+        order1.setCustomerName("Customer Name");
+        order1.setDeliveryAddress("42 Main St");
+        order1.setId(123L);
+        order1.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
+        order1.setOrderItems(new ArrayList<>());
+        order1.setOrderPrice(BigDecimal.valueOf(42L));
+
+        OrderServiceImpl orderServiceImpl = new OrderServiceImpl(orderRepository, mock(OrderItemRepository.class),
+                orderPriceCalculationService);
+        orderServiceImpl.add(order1);
+        OrderRestController orderRestController = new OrderRestController(orderServiceImpl);
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCustomerName("Customer Name");
+        orderDto.setDeliveryAddress("42 Main St");
+        orderDto.setId(123L);
+        orderDto.setOrderDateTime("2020-03-01");
+        orderDto.setOrderItemDtos(new ArrayList<>());
+        orderDto.setOrderPrice("Order Price");
+        orderRestController.createOrder(orderDto);
+    }
+
+    @Test
+    void testUpdateOrder() {
+        OrderRestController orderRestController = new OrderRestController(new OrderServiceImpl(mock(OrderRepository.class),
+                mock(OrderItemRepository.class), mock(OrderPriceCalculationService.class)));
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCustomerName("Customer Name");
+        orderDto.setDeliveryAddress("42 Main St");
+        orderDto.setId(123L);
+        orderDto.setOrderDateTime("2020-03-01");
+        orderDto.setOrderItemDtos(new ArrayList<>());
+        orderDto.setOrderPrice("Order Price");
+        orderRestController.updateOrder(orderDto);
+    }
+
+    @Test
+    @Disabled("TODO: Complete this test")
+    void testUpdateOrder2() {
+        Order order = new Order();
+        order.setCustomerName("Customer Name");
+        order.setDeliveryAddress("42 Main St");
+        order.setId(123L);
+        order.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
+        order.setOrderItems(new ArrayList<>());
+        order.setOrderPrice(BigDecimal.valueOf(42L));
+        OrderRepository orderRepository = mock(OrderRepository.class);
+        when(orderRepository.saveAndFlush((Order) any())).thenReturn(order);
+        OrderPriceCalculationService orderPriceCalculationService = mock(OrderPriceCalculationService.class);
+        when(orderPriceCalculationService.orderPriceCalculation((Order) any())).thenReturn(BigDecimal.valueOf(42L));
+
+        Order order1 = new Order();
+        order1.setCustomerName("Customer Name");
+        order1.setDeliveryAddress("42 Main St");
+        order1.setId(123L);
+        order1.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
+        order1.setOrderItems(new ArrayList<>());
+        order1.setOrderPrice(BigDecimal.valueOf(42L));
+
+        OrderServiceImpl orderServiceImpl = new OrderServiceImpl(orderRepository, mock(OrderItemRepository.class),
+                orderPriceCalculationService);
+        orderServiceImpl.add(order1);
+        OrderRestController orderRestController = new OrderRestController(orderServiceImpl);
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCustomerName("Customer Name");
+        orderDto.setDeliveryAddress("42 Main St");
+        orderDto.setId(123L);
+        orderDto.setOrderDateTime("2020-03-01");
+        orderDto.setOrderItemDtos(new ArrayList<>());
+        orderDto.setOrderPrice("Order Price");
+        orderRestController.updateOrder(orderDto);
     }
 
     @Test
@@ -102,18 +163,6 @@ class OrderRestControllerTest {
         orderItem.setCups(1);
         orderItem.setId(123L);
         when(this.orderServiceImpl.addItem((OrderItem) any())).thenReturn(orderItem);
-
-        CoffeeVariety coffeeVariety1 = new CoffeeVariety();
-        coffeeVariety1.setAvailable(true);
-        coffeeVariety1.setId(123L);
-        coffeeVariety1.setName("Name");
-        coffeeVariety1.setPrice(BigDecimal.valueOf(42L));
-
-        OrderItem orderItem1 = new OrderItem();
-        orderItem1.setCoffeeVariety(coffeeVariety1);
-        orderItem1.setCups(1);
-        orderItem1.setId(123L);
-        when(this.orderItemMapper.toModel((OrderItemDto) any())).thenReturn(orderItem1);
 
         CoffeeVarietyDto coffeeVarietyDto = new CoffeeVarietyDto();
         coffeeVarietyDto.setAvailable(true);
@@ -134,44 +183,28 @@ class OrderRestControllerTest {
                 .perform(requestBuilder)
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string(
-                                "{\"id\":123,\"coffeeVariety\":{\"id\":123,\"name\":\"Name\",\"price\":42,\"available\":true},\"cups\":1}"));
+                .andExpect(MockMvcResultMatchers.content().string("{\"id\":123,\"coffeeVariety\":null,\"cups\":1}"));
     }
 
     @Test
     void testDeleteOrderById() throws Exception {
         doNothing().when(this.orderServiceImpl).delete((Long) any());
-
-        Order order = new Order();
-        order.setCustomerName("Customer Name");
-        order.setDeliveryAddress("42 Main St");
-        order.setId(123L);
-        order.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
-        order.setOrderItems(new ArrayList<>());
-        order.setOrderPrice(BigDecimal.valueOf(42L));
-        when(this.orderMapper.toModel((OrderDto) any())).thenReturn(order);
-
-        OrderDto orderDto = new OrderDto();
-        orderDto.setCustomerName("Customer Name");
-        orderDto.setDeliveryAddress("42 Main St");
-        orderDto.setId(123L);
-        orderDto.setOrderDateTime("2020-03-01");
-        orderDto.setOrderItemDtos(new ArrayList<>());
-        orderDto.setOrderPrice("Order Price");
-        String content = (new ObjectMapper()).writeValueAsString(orderDto);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/order/delete/{id}", 123L)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/order/delete/{id}", 123L);
         MockMvcBuilders.standaloneSetup(this.orderRestController)
                 .build()
                 .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string(
-                                "{\"id\":123,\"orderDateTime\":[1,1,1,1,1],\"customerName\":\"Customer Name\",\"deliveryAddress\":\"42 Main"
-                                        + " St\",\"orderPrice\":42,\"orderItems\":[]}"));
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void testDeleteOrderById2() throws Exception {
+        doNothing().when(this.orderServiceImpl).delete((Long) any());
+        MockHttpServletRequestBuilder deleteResult = MockMvcRequestBuilders.delete("/order/delete/{id}", 123L);
+        deleteResult.contentType("https://example.org/example");
+        MockMvcBuilders.standaloneSetup(this.orderRestController)
+                .build()
+                .perform(deleteResult)
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
@@ -209,27 +242,7 @@ class OrderRestControllerTest {
         order.setOrderItems(new ArrayList<>());
         order.setOrderPrice(BigDecimal.valueOf(42L));
         when(this.orderServiceImpl.findById((Long) any())).thenReturn(order);
-
-        Order order1 = new Order();
-        order1.setCustomerName("Customer Name");
-        order1.setDeliveryAddress("42 Main St");
-        order1.setId(123L);
-        order1.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
-        order1.setOrderItems(new ArrayList<>());
-        order1.setOrderPrice(BigDecimal.valueOf(42L));
-        when(this.orderMapper.toModel((OrderDto) any())).thenReturn(order1);
-
-        OrderDto orderDto = new OrderDto();
-        orderDto.setCustomerName("Customer Name");
-        orderDto.setDeliveryAddress("42 Main St");
-        orderDto.setId(123L);
-        orderDto.setOrderDateTime("2020-03-01");
-        orderDto.setOrderItemDtos(new ArrayList<>());
-        orderDto.setOrderPrice("Order Price");
-        String content = (new ObjectMapper()).writeValueAsString(orderDto);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/order/find/{id}", 123L)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/order/find/{id}", 123L);
         MockMvcBuilders.standaloneSetup(this.orderRestController)
                 .build()
                 .perform(requestBuilder)
@@ -242,7 +255,7 @@ class OrderRestControllerTest {
     }
 
     @Test
-    void testUpdateOrder() throws Exception {
+    void testGetOrderById2() throws Exception {
         Order order = new Order();
         order.setCustomerName("Customer Name");
         order.setDeliveryAddress("42 Main St");
@@ -250,31 +263,12 @@ class OrderRestControllerTest {
         order.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
         order.setOrderItems(new ArrayList<>());
         order.setOrderPrice(BigDecimal.valueOf(42L));
-        when(this.orderServiceImpl.edit((Order) any())).thenReturn(order);
-
-        Order order1 = new Order();
-        order1.setCustomerName("Customer Name");
-        order1.setDeliveryAddress("42 Main St");
-        order1.setId(123L);
-        order1.setOrderDateTime(LocalDateTime.of(1, 1, 1, 1, 1));
-        order1.setOrderItems(new ArrayList<>());
-        order1.setOrderPrice(BigDecimal.valueOf(42L));
-        when(this.orderMapper.toModel((OrderDto) any())).thenReturn(order1);
-
-        OrderDto orderDto = new OrderDto();
-        orderDto.setCustomerName("Customer Name");
-        orderDto.setDeliveryAddress("42 Main St");
-        orderDto.setId(123L);
-        orderDto.setOrderDateTime("2020-03-01");
-        orderDto.setOrderItemDtos(new ArrayList<>());
-        orderDto.setOrderPrice("Order Price");
-        String content = (new ObjectMapper()).writeValueAsString(orderDto);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/order/update")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
+        when(this.orderServiceImpl.findById((Long) any())).thenReturn(order);
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/order/find/{id}", 123L);
+        getResult.contentType("https://example.org/example");
         MockMvcBuilders.standaloneSetup(this.orderRestController)
                 .build()
-                .perform(requestBuilder)
+                .perform(getResult)
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
                 .andExpect(MockMvcResultMatchers.content()
