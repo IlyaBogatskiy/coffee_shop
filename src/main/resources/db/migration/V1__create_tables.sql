5bd6475362786f750ea862508377db87866b79af
IlyaBogatskiy <ilya.bogatskiy@gmail.com>
2022-06-24 17:27:25 +0300

    Исправил ошибки в миграции БД.

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,23 +1,26 @@
 CREATE TABLE coffee_variety
 (
-    id                   BIGSERIAL NOT NULL PRIMARY KEY,
-    name_of_variety      VARCHAR(255),
-    price                INT,
-    variety_availability BOOLEAN
+    id                   BIGSERIAL    NOT NULL PRIMARY KEY,
+    name_of_variety      VARCHAR(255) NOT NULL,
+    price                DECIMAL,
+    variety_availability BOOLEAN      NOT NULL
 );
 
 CREATE TABLE orders
 (
-    id          BIGSERIAL NOT NULL PRIMARY KEY,
-    date        DATE,
-    customer    VARCHAR(255),
-    address     VARCHAR(255),
-    total_price INT
+    id          BIGSERIAL    NOT NULL PRIMARY KEY,
+    date        TIMESTAMP WITHOUT TIME ZONE,
+    customer    VARCHAR(255) NOT NULL,
+    address     VARCHAR(255) NOT NULL,
+    total_price DECIMAL
 );
 
 CREATE TABLE order_item
 (
-    id     BIGSERIAL NOT NULL PRIMARY KEY,
-    grade  VARCHAR(255),
-    amount INT
+    id                BIGSERIAL NOT NULL PRIMARY KEY,
+    coffee_variety_id INTEGER   NOT NULL,
+    cups              INTEGER   NOT NULL,
+    order_id          integer   NOT NULL,
+    FOREIGN KEY (coffee_variety_id) REFERENCES coffee_variety (id),
+    FOREIGN KEY (order_id) REFERENCES orders (id)
 );
